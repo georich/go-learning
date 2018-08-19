6879f91070800ece768ec6371004dcd52fbbaa30
George Richards <georich@protonmail.com>
2018-08-19 17:09:58 +0100

    Ninth exercise, level 4

---
+++ level4/exercise9/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+
+	x["richards_goerge"] = []string{"pizza", "alice", "cats"}
+
+	for k, v := range x {
+		fmt.Println("These are the values for:", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+}
