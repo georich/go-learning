7969a79ae3e4546e3d9a4b7e5e00bd56ff155a3b
George Richards <georich@protonmail.com>
2018-08-22 17:55:01 +0100

    Second exercise, level 9

---
+++ level9/exercise2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+type human interface {
+	speak() string
+}
+
+func (p *person) speak() string {
+	s := fmt.Sprintf("Hello, my name is %s %s", p.first, p.last)
+	return s
+}
+
+func saySomething(h human) {
+	fmt.Println("speak", h.speak())
+}
+
+func main() {
+	p1 := person{
+		first: "George",
+		last:  "Richards",
+	}
+	saySomething(&p1)
+	// saySomething(p1)
+}
