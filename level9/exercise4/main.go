2e63b3f6418861b9816840b97d6db5f417d21934
George Richards <georich@protonmail.com>
2018-08-24 15:48:01 +0100

    Fourth exercise, level 9

---
+++ level9/exercise4/main.go
@@ -0,0 +1,31 @@
+// race condition example
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	incrementer := 0
+	gs := 100
+	wg.Add(gs)
+	var m sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+
+			m.Lock()
+			v := incrementer
+			v++
+			incrementer = v
+			m.Unlock()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(incrementer)
+}
